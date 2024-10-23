dd1f01e74f3dd6638fb4d134a6eeb06e7e4df5bf
Chinmay <praneethsbhandaru@gmail.com>
2024-10-23 02:21:55 -0400

    Deploy to Heroku using Docker

--- ml_backend/app.py
+++ ml_backend/app.py
@@ -1,4 +1,6 @@
 # app.py
+import os
+
 from flask import Flask, jsonify
 from flask_cors import CORS
 
@@ -9,6 +11,8 @@ CORS(app)  # This will enable CORS for all routes
 def home():
     return jsonify(message="Hello from the backend!")
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5500)
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
