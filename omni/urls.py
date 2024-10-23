117172acd131623f57748f4143c8f75d8eb1dba0
Chinmay <praneethsbhandaru@gmail.com>
2024-10-23 14:43:50 -0400

    fixed detection of remotely hosted templates

--- omni/urls.py
+++ omni/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, re_path
-from django.views.generic import TemplateView
+from django.views.generic import RedirectView
 from django.conf import settings
 from django.http import HttpResponseRedirect
 
@@ -10,12 +10,17 @@ def static_redirect_view(request, path):
     redirect_url = f"{settings.STATIC_URL}{path}"
     return HttpResponseRedirect(redirect_url)
 
+# Define a custom redirect view for serving index.html from an external server
+def index_redirect_view(request):
+    # Redirect to index.html hosted on the external server
+    return HttpResponseRedirect('http://localhost:3000/index.html')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     # Redirect all static file requests to the STATIC_URL defined in settings.py
     re_path(r'^static/(?P<path>.*)$', static_redirect_view),
 
-    # Serve index.html for all other requests
-    re_path(r'^(?!static/).*$', TemplateView.as_view(template_name='index.html')),
+    # Redirect requests for the root URL and all non-static paths to the external server's index.html
+    re_path(r'^(?!static/).*$', index_redirect_view),
 ]
