e1c41632e8ccf0e91e1132ebc4e99311b2ec81ad
Chinmay <praneethsbhandaru@gmail.com>
2024-10-23 04:56:32 -0400

    testing how to get react to work with django backend and heroku

--- omni/settings.py
+++ omni/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'omni.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'frontend/build'] #BASE_DIR / 'templates',
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -117,6 +117,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Add these imports at the top
+import os
+
+# Add these settings
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend', 'build', 'static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
